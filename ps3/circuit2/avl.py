23b5bfc42b6faf9c36dc442c3ca023a49c61ab66
gskundi87 <74677259+gskundi87@users.noreply.github.com>
2022-08-17 12:50:06 -0700

    new

---
+++ ps3/circuit2/avl.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 17 11:52:14 2022
+
+@author: p4u1
+"""
+
+import bst
+
+class avl(bst.bst):
+    def __init__(self):
+        super().__init__(self)
+        
+    
